40beee7e2507486edcbd5e25134f60707a0c703a
Marcus Whybrow <git@marcuswhybrow.net>
2010-05-20 13:01:09 +0100

    Added the "blueprint" css framework and applied it to the templates in a basic form

--- keyval/core/views.py
+++ keyval/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.http import Http404
 from keyval.core.models import KeyVal
+from django.template import RequestContext
 
 def user_profile(request, username):
     """Displays the key value pairs for a user"""
@@ -13,7 +14,7 @@ def user_profile(request, username):
     
     return render_to_response('core/user_profile.html', {
         'user_profile': user,
-    })
+    }, context_instance=RequestContext(request))
 
 def keyval_profile(request, username, key):
     """Displays information regarding a key for the given username"""
@@ -31,4 +32,4 @@ def keyval_profile(request, username, key):
     return render_to_response('core/keyval_profile.html', {
         'user_profile': user,
         'keyval': keyval,
-    })
+    }, context_instance=RequestContext(request))
--- keyval/urls.py
+++ keyval/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    # Serve media through Django (not to be used in production)
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     
     url(r'^$', direct_to_template, {'template': 'core/index.html'}, name='index'),
     url(r'^account/$', direct_to_template, {'template': 'core/account.html'}, name='account'),
